f16b56228186f68c4616c9b794cb09a8939e23cc
Monster Dev <ellexidecodes@gmail.com>
2019-05-27 14:42:13 +0800

    update to ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
-// Copy import
-$('.copycode').click(function() {
-    var $elem = $(this),
-        text = $('.code').text(),
-        tempCopy = $('<input>').val(text).appendTo('body').select();
-    document.execCommand('copy');
-    tempCopy.remove();
-    $elem.text('Copied!');
-    setTimeout(() => {
-        $elem.text('Copy');
-    }, 1000);
+$(document).ready(()=>{
+    // Copy import
+    $('.copycode').click(function() {
+        var $elem = $(this),
+            text = $('.code').text(),
+            tempCopy = $('<input>').val(text).appendTo('body').select();
+        document.execCommand('copy');
+        tempCopy.remove();
+        $elem.text('Copied!');
+        setTimeout(() => {
+            $elem.text('Copy');
+        }, 1500);
+    });
 });
