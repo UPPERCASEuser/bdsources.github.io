28a9c698cc89a328be1ffa50f51aae255972f1dd
Monster Dev <ellexidecodes@gmail.com>
2019-05-27 14:33:48 +0800

    rehost and move to folders

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// Copy import
+$('.copycode').click(function() {
+    var $elem = $(this),
+        text = $('.code').text(),
+        tempCopy = $('<input>').val(text).appendTo('body').select();
+    document.execCommand('copy');
+    tempCopy.remove();
+    $elem.text('Copied!');
+    setTimeout(() => {
+        $elem.text('Copy');
+    }, 1000);
+});
